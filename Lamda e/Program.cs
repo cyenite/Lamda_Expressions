931e7aa842180a18a95ecc7ada9e1a8810fb4aa2
Akron254 <aaronrono42@gmail.com>
2018-11-03 12:22:09 +0300

    Initial commit

---
+++ Lamda e/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lamda_e
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var names = new List<string>();
+
+            for (int a = 0; a < 100; a++)
+            {
+                //  names.Add($"Name{a}");
+                names.Add("Name" + a);
+            }
+
+            var result = names.Where(w => (w.EndsWith("2")));
+            //var result = names.Where(My_Function);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
